c85351a073bac7399b558cb26b6e46607286bfe4
Gao Chaoyu <bikegcy@gmail.com>
2017-10-03 04:56:56 -0700

    test for Room

---
+++ assignment2/RoomTest.java
@@ -0,0 +1,40 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xwenfei on 10/2/17.
+ */
+public class RoomTest {
+    @Test
+    public void setTimeOccupied() throws Exception {
+
+    }
+
+    private Room room1;
+    private Room room2;
+    private Room room3;
+    private MyPriorityQueue<Room> RoomQueue;
+
+    @Before
+    public void setUp() throws Exception {
+
+        room1 = new Room();
+        room2 = new Room();
+        room3 = new Room();
+        RoomQueue = new MyPriorityQueue<Room>();
+
+        room1.setTimeOccupied(30);
+        room2.setTimeOccupied(20);
+        room3.setTimeOccupied(40);
+
+        RoomQueue.insert(room1);
+        RoomQueue.insert(room2);
+        RoomQueue.insert(room3);
+
+        assertEquals(RoomQueue.front(), room2);
+
+    }
+
+}
