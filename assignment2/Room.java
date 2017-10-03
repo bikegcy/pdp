5dc727fea1fe9d98c4935812691f3031512ba4f2
Gao Chaoyu <bikegcy@gmail.com>
2017-10-03 04:56:34 -0700

    Create Room class

---
+++ assignment2/Room.java
@@ -0,0 +1,24 @@
+
+/**
+ * created by xwenfei at 09/28/17
+ */
+
+public class Room implements Comparable<Room> {
+    public int TimeOccupied;
+
+    public Room(){
+        this.TimeOccupied = 0;
+    }
+
+    public void setTimeOccupied(int TreatmentTime){
+        this.TimeOccupied = this.TimeOccupied + TreatmentTime;
+    }
+
+
+    public int compareTo(Room room) {
+        if(room == null){
+            throw new IllegalArgumentException("room to compare is null");
+        }
+        return this.TimeOccupied - room.TimeOccupied;
+    }
+}
